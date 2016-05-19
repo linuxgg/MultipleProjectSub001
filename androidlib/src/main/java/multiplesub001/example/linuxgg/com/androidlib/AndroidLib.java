ed06275b921f3dddabfcd84beea80a1473b21b43
linuxgg <qdtom@foxmail.com>
2016-05-20 00:10:06 +0800

    add android lib

---
+++ androidlib/src/main/java/multiplesub001/example/linuxgg/com/androidlib/AndroidLib.java
@@ -0,0 +1,10 @@
+package multiplesub001.example.linuxgg.com.androidlib;
+
+/**
+ * Created by tom on 16-5-20.
+ */
+public class AndroidLib {
+    public static String TomTest() {
+        return "Android lib";
+    }
+}
