8df31dcf5e59f9c2ebc203584b02322a0751e2ab
linuxgg <qdtom@foxmail.com>
2016-05-20 00:10:15 +0800

    add android lib

--- java_lib/src/main/java/com/example/MyClass.java
+++ java_lib/src/main/java/com/example/MyClass.java
@@ -1,9 +1,8 @@
 package com.example;
 
-import sun.plugin2.util.SystemUtil;
 
 public class MyClass {
-    public static void MyTest() {
-        System.out.print("MyTest");
+    public static String MyTest() {
+        return "Tom test";
     }
 }
